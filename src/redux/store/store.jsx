414debefcf30b9248772e6d1352253d43d9e6617
amd200 <ahm12d.m@gmail.com>
2024-12-31 20:47:55 +0200

    store

--- src/redux/store/store.jsx
+++ src/redux/store/store.jsx
@@ -4,5 +4,7 @@ import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const middleware = [thunk];
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
+
+const store = createStore(rootReducer, process.env.REACT_APP_ENV === "development" ? composeWithDevTools(applyMiddleware(...middleware)) : applyMiddleware(...middleware));
+
 export default store;
